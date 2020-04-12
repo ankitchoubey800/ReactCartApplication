a710b214494a3247ffd710ab21f3e1a8cc5e1646
Ankit Choubey <ankitchoubey.800@gmail.com>
2020-04-12 13:29:54 +0530

    Updates

---
+++ precache-manifest.4ea45e10c771a502455c3f99c1f4e329.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62dcf4c24d22e95dbd26691193b5a61",
+    "url": "/ReactCartApplication/index.html"
+  },
+  {
+    "revision": "02d98ca88fce34d0bfcc",
+    "url": "/ReactCartApplication/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cf7e8319a582c2040f81",
+    "url": "/ReactCartApplication/static/css/main.74f83179.chunk.css"
+  },
+  {
+    "revision": "02d98ca88fce34d0bfcc",
+    "url": "/ReactCartApplication/static/js/2.0e267cf9.chunk.js"
+  },
+  {
+    "revision": "251f5e47702f42bf144e6765e8c1f80e",
+    "url": "/ReactCartApplication/static/js/2.0e267cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7e8319a582c2040f81",
+    "url": "/ReactCartApplication/static/js/main.f588c934.chunk.js"
+  },
+  {
+    "revision": "3a9d13c55af507f93006",
+    "url": "/ReactCartApplication/static/js/runtime-main.39af722a.js"
+  },
+  {
+    "revision": "4da11fede2dfc4a2bea40068c824f370",
+    "url": "/ReactCartApplication/static/media/headphone-1868612_640.4da11fed.jpg"
+  },
+  {
+    "revision": "a007b2d13a48a9b6b55f4b66ca365a00",
+    "url": "/ReactCartApplication/static/media/home-office-336373_640.a007b2d1.jpg"
+  }
+]);
