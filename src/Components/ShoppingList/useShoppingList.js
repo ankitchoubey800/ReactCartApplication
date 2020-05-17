944673f1c86fd79f9fe0e761a2bc2c9288065e03
Ankit Choubey <ankitchoubey.800@gmail.com>
2020-05-17 19:40:10 +0530

    using local json

--- src/Components/ShoppingList/useShoppingList.js
+++ src/Components/ShoppingList/useShoppingList.js
@@ -7,15 +7,15 @@ const useShoppingList = () => {
 
     useEffect(() => {
         GetShoppingItems().then(result => {
-             setItems(result.items);
-            // setItems([{ "name": "item 1", "price": { "actual": 1000, "display": 1900 }, "discount": 10 },
-            // { "name": "item 2", "price": { "actual": 3000, "display": 3900 }, "discount": 30 },
-            // { "name": "item 3", "price": { "actual": 2000, "display": 2900 }, "discount": 50 },
-            // { "name": "item 4", "price": { "actual": 5000, "display": 5900 }, "discount": 40 },
-            // { "name": "item 5", "price": { "actual": 1500, "display": 2900 }, "discount": 20 },
-            // { "name": "item 6", "price": { "actual": 4500, "display": 6900 }, "discount": 60 },
-            // { "name": "item 7", "price": { "actual": 2300, "display": 3900 }, "discount": 35 },
-            // { "name": "item 8", "price": { "actual": 7000, "display": 9900 }, "discount": 45 }]);
+            //  setItems(result.items);
+            setItems([{ "name": "item 1", "price": { "actual": 1000, "display": 1900 }, "discount": 10 },
+            { "name": "item 2", "price": { "actual": 3000, "display": 3900 }, "discount": 30 },
+            { "name": "item 3", "price": { "actual": 2000, "display": 2900 }, "discount": 50 },
+            { "name": "item 4", "price": { "actual": 5000, "display": 5900 }, "discount": 40 },
+            { "name": "item 5", "price": { "actual": 1500, "display": 2900 }, "discount": 20 },
+            { "name": "item 6", "price": { "actual": 4500, "display": 6900 }, "discount": 60 },
+            { "name": "item 7", "price": { "actual": 2300, "display": 3900 }, "discount": 35 },
+            { "name": "item 8", "price": { "actual": 7000, "display": 9900 }, "discount": 45 }]);
         });
     }, []);
 
@@ -35,15 +35,15 @@ const useShoppingList = () => {
             }
             else {
                 GetShoppingItems().then(result => {
-                    setItems(result.items);
-                    // setItems([{ "name": "item 1", "price": { "actual": 1000, "display": 1900 }, "discount": 10 },
-                    // { "name": "item 2", "price": { "actual": 3000, "display": 3900 }, "discount": 30 },
-                    // { "name": "item 3", "price": { "actual": 2000, "display": 2900 }, "discount": 50 },
-                    // { "name": "item 4", "price": { "actual": 5000, "display": 5900 }, "discount": 40 },
-                    // { "name": "item 5", "price": { "actual": 1500, "display": 2900 }, "discount": 20 },
-                    // { "name": "item 6", "price": { "actual": 4500, "display": 6900 }, "discount": 60 },
-                    // { "name": "item 7", "price": { "actual": 2300, "display": 3900 }, "discount": 35 },
-                    // { "name": "item 8", "price": { "actual": 7000, "display": 9900 }, "discount": 45 }]);
+                    // setItems(result.items);
+                    setItems([{ "name": "item 1", "price": { "actual": 1000, "display": 1900 }, "discount": 10 },
+                    { "name": "item 2", "price": { "actual": 3000, "display": 3900 }, "discount": 30 },
+                    { "name": "item 3", "price": { "actual": 2000, "display": 2900 }, "discount": 50 },
+                    { "name": "item 4", "price": { "actual": 5000, "display": 5900 }, "discount": 40 },
+                    { "name": "item 5", "price": { "actual": 1500, "display": 2900 }, "discount": 20 },
+                    { "name": "item 6", "price": { "actual": 4500, "display": 6900 }, "discount": 60 },
+                    { "name": "item 7", "price": { "actual": 2300, "display": 3900 }, "discount": 35 },
+                    { "name": "item 8", "price": { "actual": 7000, "display": 9900 }, "discount": 45 }]);
                 });
             }
         }
